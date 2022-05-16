2ed963d592614798a59f902812dfdc377604e797
Sebulba <egor.shobanov@gmail.com>
2022-05-16 21:02:37 +0300

    100%

--- FRONTEND/main.py
+++ FRONTEND/main.py
@@ -4,4 +4,4 @@ app = create_app()
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='192.168.50.191', debug=True)
--- FRONTEND/website/auth.py
+++ FRONTEND/website/auth.py
@@ -14,7 +14,7 @@ def db_auth():
     return connect.cursor()
 
 
-def add_comma(num: int) -> str:
+def add_comma(num: [int, str]) -> str:
     new_num = ''
     num = str(num)
     count = 1
@@ -62,6 +62,7 @@ def d_2020():
 
     income_source_value = dict()
     income_source_perc = dict()
+    income_values = dict()
     for income_source in ('Advertising', 'Asset sale', 'Licensing', 'Renting', 'Subscription', 'Usage fees'):
         cursor.execute('''
             select SUM(Income)
@@ -72,8 +73,18 @@ def d_2020():
 
         income_from_source = cursor.fetchone()[0]
 
-        income_source_value[income_source] = int(income_from_source)
+        income_values[income_source] = int(income_from_source)
+
+        income_source_value[income_source] = add_comma(int(income_from_source))
         income_source_perc[income_source] = round(income_from_source / total_income * 100)
+
+    income_value_max = max(income_values, key=lambda k: income_values.get(k))
+
+    cursor.execute('''
+        select distinct [Income Breakdowns]
+        from [Financial Statistics]
+        where Year = ?
+    ''', year)
 
     breakdowns_value = dict()
     breakdowns_perc = dict()
@@ -87,7 +98,7 @@ def d_2020():
 
         income_from_breakdown = cursor.fetchone()[0]
 
-        breakdowns_value[breakdown] = round(income_from_breakdown)
+        breakdowns_value[breakdown] = add_comma(round(income_from_breakdown))
         breakdowns_perc[breakdown] = round(income_from_breakdown / total_income * 100)
 
     quantity_value = dict()
@@ -134,10 +145,10 @@ def d_2020():
 
         monthly_income[month] = int(cursor.fetchone()[0])
 
-    avg_monthly_income = round(sum(monthly_income.values()) / len(monthly_income))
+    avg_monthly_income = add_comma(round(sum(monthly_income.values()) / len(monthly_income)))
 
     monthly_profit = dict()
-    for month in ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'][::-1]:
+    for month in ['Dec', 'Nov', 'Oct', 'Sep', 'Aug', 'Jul', 'Jun', 'May', 'Apr', 'Mar', 'Feb', 'Jan']:
         cursor.execute('''
             select sum([Operating profit])
             from [Financial Statistics]
@@ -153,7 +164,7 @@ def d_2020():
             where Year = ?
         ''', year)
 
-    total_profit = int(cursor.fetchone()[0])
+    total_profit = add_comma(int(cursor.fetchone()[0]))
 
     cursor.execute('''
             select sum(Income)
@@ -180,7 +191,13 @@ def d_2020():
     for key, value in zip(quantity_value.keys(), quantity_value.values()):
         quantity_value[key] = add_comma(value)
 
-    return render_template("Income Sources 2020.html", target_income_comm=target_income_comm, total_income_comm=total_income_comm, income_achieved=income_achieved, total_income=total_income,
+    colors = ['white', 'black']
+    background = ['#1D1D3A', 'white']
+
+    return render_template("Income Sources 2020.html", income_value_max=income_value_max, year=year,
+                           background=background, colors=colors, target_income_comm=target_income_comm,
+                           total_income_comm=total_income_comm, income_achieved=income_achieved,
+                           total_income=total_income,
                            total_quantity=total_quantity, quantity_value=quantity_value,
                            income_source_value=income_source_value,
                            income_source_perc=income_source_perc, breakdowns_value=breakdowns_value,
